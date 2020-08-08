5c1231f4f41dacdaf8e8ff988bfd8741c561249c
Uriya <uriyak2@gmail.com>
2020-08-08 19:25:17 +1200

    Clean excessive newlines

--- blocker.py
+++ blocker.py
@@ -1,13 +1,11 @@
 import re
-
 HOSTS_PATH = r'C:\Windows\System32\drivers\etc\hosts'
-DOMAINS = ['facebook.com', 'ynet.co.il', 'walla.co.il']
-
 PATTERN_TEMPLATE = '127\.0\.0\.1.+({domains})'
+DOMAINS = ['facebook.com', 'ynet.co.il', 'walla.co.il']
 BLOCK_LINES = '127.0.0.1 {d}\n127.0.0.1 www.{d}'
 NL = '\n'
 
-pattern = PATTERN_TEMPLATE.format(domains='|'.join(re.escape(d) for d in DOMAINS))
+pattern = PATTERN_TEMPLATE.format(domains='|'.join(map(re.escape, DOMAINS)))
 
 with open(HOSTS_PATH, 'r+',) as f:
     data = f.read()
@@ -16,5 +14,6 @@ with open(HOSTS_PATH, 'r+',) as f:
         new_data = re.sub(pattern, '', data)
     else:
         new_data = data + NL + NL.join(BLOCK_LINES.format(d=d) for d in DOMAINS)
-    f.write(new_data.strip(NL) + NL)
+    new_data = re.sub(r'\n{3,}', NL+NL, new_data).strip()
+    f.write(new_data)
     f.truncate()
